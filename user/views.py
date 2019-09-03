a32b851293453398af64b525a93751cc2824c478
zhangxwww <631133544@qq.com>
2019-09-03 09:33:04 +0800

    init

---
+++ user/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+
+# Create your views here.
+def logon(request):
+    pass
+
+def login(request):
+    pass
+
+def logout(request):
+    pass
