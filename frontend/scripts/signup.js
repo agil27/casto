e31e754d9754fffdccdd3a0e807383f5f6d3b41c
agil27 <wang-yb16@mails.tsinghua.edu.cn>
2019-09-04 23:34:30 +0800

    Create signup.js

---
+++ frontend/scripts/signup.js
@@ -0,0 +1,22 @@
+function onSignupClicked() {
+    console.log("Signup Button Clicked")
+    let name = $("#username")
+    let pwd = $("#password")
+    if (length(username) == 0 || length(password) == 0) {
+        return
+    }
+    else {
+        let data = {
+            username: name,
+            password: pwd
+        }
+        $.post("dashboard.html", data, function(res) {
+            console.log(res)
+        })
+    }
+}
+
+$(document).ready(function() {
+    let signup_btn = $("#btnSignup")
+    signup_btn.click(onSignupClicked)
+})
