8b1f724b1807eff65d984cc48048fd622031ec07
zhangxwww <631133544@qq.com>
2019-09-07 15:27:30 +0800

    time picker in admin/dashboard

--- backend/user/static/admin/scripts/dashboard.js
+++ backend/user/static/admin/scripts/dashboard.js
@@ -5,41 +5,6 @@ function GetQueryString(name) {
     return null;
 }
 
-/*
-function setDetectBtn(op_id) {
-    $("#detect_btn").click(function() {
-        $("#status").text("正在进行图片转换，请稍候……")
-        $("#function_block").hide()
-        $.post(
-            "../operation/0/net/",
-            {"id": op_id},
-            function(res) {
-                console.log(res.data)
-                $("#preview").attr("src", "../" + res.data)
-                $("#status").text("已完成转换，刷新页面可以查看操作记录")
-            }
-        )
-    })
-}
-*/
-/*
-function setGenderBtn(op_id) {
-    $("#gender_btn").click(function () {
-        $("#status").text("正在进行图片转换，请稍候……")
-        $("#function_block").hide()
-        $.post(
-            "../operation/1/net/",
-            {"id": op_id},
-            function (res) {
-                console.log(res.data)
-                $("#preview").attr("src", "../" + res.data)
-                $("#status").text("已完成转换，刷新页面可以查看操作记录")
-            }
-        )
-    })
-}
-*/
-
 $(document).ready(function () {
     let explore_btn = $("#explore_btn")
     let upload_btn = $("#upload_btn")
@@ -51,75 +16,6 @@ $(document).ready(function () {
     let delete_all_btn = $("#delete-all-btn")
 
     $("#function_block").hide()
-
-    /*
-    explore_btn.click(function () {
-        file_input.click()
-    })
-    */
-
-    /*
-    file_input.change(function () {
-        file_addr.val(file_input.val())
-        let formData = new FormData();
-        formData.append("image", file_input[0].files[0]);
-        $.ajax({
-            url: "../operation/upload/",
-            type: 'post',
-            data: formData,
-            contentType: false,
-            processData: false,
-            success: function (res) {
-                console.log(res)
-                if (res.addr) {
-                    $("#preview").attr("src", "../static/" + res.addr)
-                    upload_btn.attr("disabled", true)
-                    file_addr.attr("disabled", true)
-                    explore_btn.attr("disabled", true)
-                    file_addr.attr("disabled", true)
-                    $("#file_desc").text("本地图片已经上传")
-                    $("#web_desc").text("已禁用网络上传")
-                    $("#status").text("图片已经上传，请选择您的服务")
-                    $("#function_block").show()
-                    setDetectBtn(res.id)
-                    setGenderBtn(res.id)
-                }
-                else if (res.error) {
-                    $("#file_desc").text("本地图片上传失败，请重新选择")
-                }
-            }
-        })
-    })
-    */
-    /*
-    upload_btn.click(function () {
-        let file_web = web_addr.val()
-        console.log(file_web)
-        $.post(
-            "../operation/upload/",
-            {"url": file_web},
-            function (res) {
-                console.log(res)
-                if (res.addr) {
-                    $("#preview").attr("src", "../static/" + res.addr)
-                    upload_btn.attr("disabled", true)
-                    file_addr.attr("disabled", true)
-                    explore_btn.attr("disabled", true)
-                    file_addr.attr("disabled", true)
-                    $("#file_desc").text("已禁用本地上传")
-                    $("#web_desc").text("网络图片已上传")
-                    $("#status").text("图片已经上传，请选择您的服务")
-                    $("#function_block").show()
-                    setDetectBtn(res.id)
-                    setGenderBtn(res.id)
-                }
-                else if (res.error) {
-                    $("#web_desc").text("网络图片上传失败，请换一张")
-                }
-            }
-        )
-    })
-    */
 
     logout_btn.click(function () {
         window.location.replace("../../logout/")
@@ -159,4 +55,22 @@ $(document).ready(function () {
     $("#checkall").click(function () {
         $(".checkbox").prop("checked", this.checked)
     })
+
+
+    $("#query-btn").click(function() {
+        date1 = $("#date1").val()
+        date2 = $("#date2").val()
+        console.log(date1, date2)
+        if (date1.length > 0 && date2.length > 0) {
+            href_path = "../dashboard/?range=yes"
+            href_time1 = "&start=" + date1
+            href_time2 = "&end=" + date2
+            window.location.replace(href_path + href_time1 + href_time2)
+        }
+    })
+
+    if (GetQueryString("range") === "yes") {
+        $("#timequery").addClass("active")
+        $("#allrecord").removeClass("active")
+    }
 })
